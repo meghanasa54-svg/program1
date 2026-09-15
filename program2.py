e6d6c1d47d8728439a16014fa5cf9e279758713f
Meghana <meghanasa54@gamail.com>
2026-09-15 11:49:44 +0530

    Add python program to swap two numbers

---
+++ program2.py
@@ -0,0 +1,15 @@
+# Program to swap two numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Before swapping:")
+print("First number:", a)
+print("Second number:", b)
+
+# Swapping
+a, b = b, a
+
+print("After swapping:")
+print("First number:", a)
+print("Second number:", b)
